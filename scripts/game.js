df9dad97921a763160f8bcc5a585efaaf385bf8d
james sewell <jamesjosephsewell@gmail.com>
2017-02-15 13:55:02 -0600

    added minimal player shooting system

--- scripts/game.js
+++ scripts/game.js
@@ -38,6 +38,20 @@
 			this.bodies.push(body)
 		}
 	}
+	var Bullet = function(center, velocity) {
+
+		this.size = { x: 3, y: 3}
+		this.center = center
+		this.velocity = velocity
+	}
+
+	Bullet.prototype = {
+		update: function(){
+			this.center.x += this.velocity.x
+			this.center.y += this.velocity.y
+
+		}
+	}
 
 	var Player = function(game, gameSize) {
 		this.game = game
